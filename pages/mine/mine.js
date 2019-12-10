868d2c789e5de1f81a2322d8b70ab5772fadf33a
unknown <unknown@example.com>
2019-12-10 17:25:51 +0800

    2019.12.10

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -9,4 +9,77 @@ Component({
       }
     }
   },
+
+
+
+  data: {
+    user: {
+      head: '/image/touxaing.png',
+      account: '123456789',
+      id: '321654'
+    },
+    money: [{
+      img: '/image/0.2切图/积分.png',
+      name: '积分',
+      num: 1000
+    }, {
+      img: '/image/0.2切图/余额.png',
+      name: '余额',
+      num: 1000
+    }, {
+      img: '/image/0.2切图/卡券.png',
+      name: '卡券',
+      num: 1000
+    }],
+    action: [{
+      title: '全部订单',
+      sub: '查看全部',
+      child: [{
+        img: '/image/0.2切图/待付款.png',
+        name: '待付款'
+      }, {
+        img: '/image/0.2切图/待发货.png',
+        name: '待发货'
+      }, {
+        img: '/image/0.2切图/待收货.png',
+        name: '待收货'
+      }, {
+        img: '/image/0.2切图/待评价.png',
+        name: '待评价'
+      }]
+    }, {
+      title: '我的服务',
+      sub: '',
+      child: [{
+        img: '/image/0.2切图/退款.png',
+        name: '我的退款'
+      }, {
+        img: '/image/0.2切图/我的评价.png',
+        name: '我的评价'
+      }, {
+        img: '/image/0.2切图/我的收藏.png',
+        name: '我的收藏'
+      }, {
+        img: '/image/0.2切图/地址管理.png',
+        name: '地址管理'
+      }]
+    }, {
+      title: '商家',
+      sub: '',
+      child: [{
+        img: '/image/0.2切图/商家信息.png',
+        name: '商家信息'
+      }, {
+        img: '/image/0.2切图/联络商家.png',
+        name: '联络商家'
+      }]
+    }]
+  },
+  methods: {
+    toOrder: function () {
+      wx.navigateTo({
+        url: '/pages/order/order',
+      })
+    }
+  }
 })
---
+++ pages/order/order.js
@@ -0,0 +1,92 @@
+// pages/order/order.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    radioItems: [
+      { name: '全部', value: '0', checked: true },
+      { name: '待付款', value: '1'},
+      { name: '待发货', value: '2' },
+      { name: '待收货', value: '3'},
+      { name: '待评价', value: '4' },
+    ],
+    orderList: [{
+      name:'商品名称111商品名称111商品名称111商品名称111商品名称111商品名称111商品名称111商品名称111',
+      img:'/image/组47.png',
+      orderNum:'123456789987',
+      itemSet:'商品规格商品规格商品规格商品规格商品规格商品规格商品规格商品规格商品规格商品规格商品规格商品规格',
+      time:'2019-12-12 12:12',
+      type:0,   //订单状态：0待付款，1待发货，2待收货，3待评价，4已完成，5关闭交易
+    }]
+  },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
